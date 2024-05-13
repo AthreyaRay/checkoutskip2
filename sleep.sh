498b8df18593125464da9e2532d61760eb16f85b
athreya <athreya@buildkite.com>
2024-05-13 13:41:49 -0400

    testing group custom commits 11

--- sleep.sh
+++ sleep.sh
@@ -2,5 +2,5 @@
 # This script waits for 10 minutes.
 
 echo "Waiting for 10 minutes..."
-sleep 600  # Sleep takes time in seconds, 10 minutes = 600 seconds
+exit 1 # Sleep takes time in seconds, 10 minutes = 600 seconds
 echo "Done waiting!"
